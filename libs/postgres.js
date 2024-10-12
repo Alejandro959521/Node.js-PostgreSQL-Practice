a7d578fe0a03c74b7d587a5200f37bfbee28fe14
Miguel Ojeda <cabrerasm081@gmail.com>
2024-10-12 13:06:51 -0400

    Done

---
+++ libs/postgres.js
@@ -0,0 +1,20 @@
+const {Client} = require('pg');
+
+async function getConnection(){
+
+  const client = new Client({
+
+    host: 'localhost',
+    post: 5432,
+    user:'nico',
+    password: 'admin123',
+    database: 'my_store'
+
+  });
+
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
+
